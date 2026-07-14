72232abf2ac2631876d7b1873125969750ca23b3
aba-geomdan <abageomdan@gmail.com>
2026-07-14 09:36:29 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { obfuscator } from "vite-plugin-javascript-obfuscator";
+import obfuscator from "vite-plugin-javascript-obfuscator";
 
 // GitHub Pages: https://aba-geomdan.github.io/esdm/ 로 배포하므로 base = "/esdm/"
 // 저장소 이름을 바꾸면 이 값도 같이 바꿔야 함.
@@ -10,6 +10,8 @@ export default defineConfig({
     obfuscator({
       apply: "build",
       include: [/\.js$/],
+      exclude: [/node_modules/],
+      debugger: false,
       options: {
         compact: true,
         stringArray: true,
